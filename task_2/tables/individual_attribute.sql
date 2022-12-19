dd60301a922141af4a7a1d25455cfccfccb2353a
Dmitry <46959388+SolonnikovDV@users.noreply.github.com>
2022-12-19 17:02:30 +0300

    Update individual_attribute.sql

--- task_2/tables/individual_attribute.sql
+++ task_2/tables/individual_attribute.sql
@@ -11,14 +11,17 @@ CREATE TABLE "public"."individual_attribute" (
 -- -------------------------------------------------------------
 
 -- CREATE INDEX "index_location_id_f_key1" ---------------------
-CREATE INDEX "index_location_id_f_key1" ON "public"."individual_attribute" USING btree( "location_id_f_key" );
+-- search agent location
+CREATE INDEX "index_location_id_f_key" ON "public"."individual_attribute" USING btree( "location_id_f_key" );
 -- -------------------------------------------------------------
 
 -- CREATE INDEX "index_inn" ------------------------------------
+--search agent attribute
 CREATE INDEX "index_inn" ON "public"."individual_attribute" USING btree( "inn" );
 -- -------------------------------------------------------------
 
 -- CREATE INDEX "index_agent_type_f_key" -----------------------
+--find type agent: company / individual
 CREATE INDEX "index_agent_type_f_key" ON "public"."individual_attribute" USING btree( "agent_type_f_key" );
 -- -------------------------------------------------------------
 
