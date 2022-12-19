addfcebc932d725b0048872d8f69de42b0112c4b
Dmitry <46959388+SolonnikovDV@users.noreply.github.com>
2022-12-19 23:16:30 +0300

    Update stock.sql

--- task_2/tables/stock.sql
+++ task_2/tables/stock.sql
@@ -24,13 +24,31 @@ CREATE INDEX "index_location_f_key" ON "public"."stock" USING btree( "location_f
 
 -- CREATE INDEX "index_product_id_f_key" -----------------------
 -- search product name + also searching by id
-CREATE INDEX "index_product_id_name_f_key" 
+CREATE INDEX "index_product_id_name_f_key"
 	ON "public"."stock" USING btree( "product_id_f_key" Asc NULLS Last, "product_name_f_key" Asc NULLS Last;
 -- -------------------------------------------------------------
 
 -- CREATE INDEX "index_transaction_in_f_key" -------------------
 -- find transaction with all attributes are comming to a storage place
 CREATE INDEX "index_transaction_in_f_key" ON "public"."stock" USING btree( "transaction_in_f_key" );
+-- -------------------------------------------------------------
+
+-- CREATE FOREIGN KEYS -----------------------------------------
+alter table public.stock
+    add constraint product_id_f_key
+        foreign key (product_id_f_key) references public.product;
+
+alter table public.stock
+    add constraint stock_location_location_id_fk
+        foreign key (location_f_key) references public.location;
+
+alter table public.stock
+    add constraint transaction_in_f_key
+        foreign key (transaction_in_f_key) references public.supply_order;
+
+alter table public.stock
+    add constraint transaction_out_f_key
+        foreign key (transaction_out_f_key) references public.purchase_order;
 -- -------------------------------------------------------------
 
 COMMIT;
