781843e095a024cc6822560d5bb02d4fe564a7c3
Dmitry <46959388+SolonnikovDV@users.noreply.github.com>
2022-12-19 23:12:27 +0300

    Update price.sql

--- task_2/tables/price.sql
+++ task_2/tables/price.sql
@@ -1,30 +1,64 @@
 BEGIN;
 
--- CREATE TABLE "price" ----------------------------------------
-CREATE TABLE "public"."price" (
-	"product_Id_f_key" Integer NOT NULL,
-	"base_price" Integer,
-	"sale_price" Integer,
-	"base_promo_price" Integer,
-	"is_final_price" Boolean,
-	"price_id" Integer NOT NULL,
-	PRIMARY KEY ( "price_id" ),
-	CONSTRAINT "unique_price_product_Id" UNIQUE( "product_Id_f_key" ),
-	CONSTRAINT "check_price_base_price" CHECK(base_price >= 0),
-	CONSTRAINT "check_price_promo_price" CHECK(base_promo_price >= 0),
-	CONSTRAINT "check_price_sale_price" CHECK(sale_price >= 0) );
+-- CREATE TABLE "product" --------------------------------------
+CREATE TABLE "public"."product" (
+	"product_id" Integer NOT NULL,
+	"name_ru" Character Varying NOT NULL,
+	"name_eng" Character Varying,
+	"brand_f_key" Integer,
+	"category_f_id" Integer,
+	"status_f_key" Integer,
+	"rating_id_f_key" Integer,
+	"product_properties_id_f_key" Integer,
+	PRIMARY KEY ( "product_id" ),
+	CONSTRAINT "unique_product_product_id" UNIQUE( "product_id" ) );
  ;
 -- -------------------------------------------------------------
 
--- CREATE INDEX "index_base_price" -----------------------------
--- searching for all types of prices
-CREATE INDEX "index_base_price" ON "public"."price" USING btree( "base_price" Asc NULLS Last, "base_promo_price" Asc NULLS Last, "sale_price" Asc NULLS Last );
+-- CREATE INDEX "index_name_brand_category" --------------------
+-- product name, brand, category mostly using together in a quieryes
+CREATE INDEX "index_name_brand_category"
+	ON "public"."product" USING btree( "name_ru" Asc NULLS Last, "brand_f_key" Asc NULLS Last, "category_f_id" Asc NULLS Last );
 -- -------------------------------------------------------------
 
--- CREATE FOREIGN KEYS -----------------------------------------
-alter table public.price
-    add constraint price_product_product_id_fk
-        foreign key ("product_Id_f_key") references public.product;
+-- CREATE INDEX "index_status_f_key" ---------------------------
+-- filtering by status sold_out / avalliable
+CREATE INDEX "index_status_f_key"
+	ON "public"."product" USING btree( "status_f_key" );
+-- -------------------------------------------------------------
+
+-- CREATE INDEX "index_rating_id_f_key" ------------------------
+-- sort by rating
+CREATE INDEX "index_rating_id_f_key"
+	ON "public"."product" USING btree( "rating_id_f_key" );
+-- -------------------------------------------------------------
+
+-- CREATE INDEX "index_properties_id_f_key" --------------------
+-- find by product properties
+CREATE INDEX "index_properties_id_f_key"
+	ON "public"."product" USING btree( "product_properties_id_f_key" );
+-- -------------------------------------------------------------
+
+-- CREATE FOREIGN KEYS for "product" table ------------------------
+alter table public.product
+    add constraint brand_f_key
+        foreign key (brand_f_key) references public.brand;
+
+alter table public.product
+    add constraint category_f_id
+        foreign key (category_f_id) references public.category;
+
+alter table public.product
+    add constraint product_properties_id_f_key
+        foreign key (product_properties_id_f_key) references public.product_properties;
+
+alter table public.product
+    add constraint rating_id_f_key
+        foreign key (rating_id_f_key) references public.rating;
+
+alter table public.product
+    add constraint status_f_key
+        foreign key (status_f_key) references public.status;
 -- -------------------------------------------------------------
 
 COMMIT;
